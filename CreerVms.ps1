0399d9d6023e53d266e7edd044755d5e7701823c
jlno-dev <63147914+jlno-dev@users.noreply.github.com>
2021-06-05 18:45:57 +0200

    Fisrt steps with powershell: read xml file create vbox

--- CreerVms.ps1
+++ CreerVms.ps1
@@ -1,3 +1,12 @@
+function Executer-VBoxCmde {
+    Param (
+     [string] $pArguments
+    )
+
+    Start-Process -NoNewWindow -FilePath "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" -ArgumentList "$pArguments"
+}
+
+<# 
 function Ajouter-Machine {
     Param (
      [string] $NomMachine,
@@ -7,9 +16,9 @@ function Ajouter-Machine {
      [int] $NumDisque
     )
 
-    VBoxManage modifyvm $NomMachine  --ioapic on --chipset ich9 --memory $RamMo --vram 128 --rtcuseutc on --boot1 dvd --boot2 disk
-    VBoxManage modifyvm $NomMachine --graphicscontroller vmsvga
-}
+   VBoxManage modifyvm $NomMachine  --ioapic on --chipset ich9 --memory $RamMo --vram 128 --rtcuseutc on --boot1 dvd --boot2 disk
+   VBoxManage modifyvm $NomMachine --graphicscontroller vmsvga
+} #>
 
 function Ajouter-DisqueSata {
     Param (
@@ -24,9 +33,22 @@ function Ajouter-DisqueSata {
      write-host "VBoxManage storageattach "$NomMachine" --storagectl ""SATA Controller"" --port "$NumPort" --device "$NumDisque" --type hdd --medium  "$NomFichier
 }
 
-$ImportData = Import-Csv "D:\projets\vbox\admin\modele_lin-orl.csv" -delimiter ";"
-foreach ( $Data in  $ImportData )
-{
-	$fichier_complet = $Data.repertoire_base + "\" + $Data.machine + "_" + $Data.fichier
-     Ajouter-DisqueSata $Data.machine $fichier_complet $Data.taille $Data.port $Data.device
-} 
+# $ImportData = Import-Csv "modele_lin-orl.csv" -delimiter ";"
+# foreach ( $Data in  $ImportData )
+# {
+# 	$fichier_complet = $Data.machine + "_" + $Data.fichier
+#      Ajouter-DisqueSata $Data.machine $fichier_complet $Data.taille $Data.port $Data.device
+# } 
+#$NomMachine = "lin-orl-002"
+# $RamMo = 4096
+#Executer-VBoxCmde "createvm --name $NomMachine  --ostype Oracle_64 --register --basefolder H:\projets\VBox\Vms"
+#Lecture du fichier.
+[xml]$xml = get-content ("H:\projets\scripts\pws\lin-orl-002.xml")
+
+#Je récupéré dans $Livres les données entre la balise <Livres> et </Livres>.
+$machine = $xml.machine
+
+#Puis j'affiche mes données.
+Write-Host $machine.nom
+Write-Host $machine.os_type
+Write-Host $machine.disques[0]
