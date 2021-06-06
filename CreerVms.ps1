14408cb41c3e91ae70fb767e6b466331b47ce361
jlno-dev <jlno-dev@outlook.fr>
2021-06-06 21:49:49 +0200

    reading xml file adding vm

--- CreerVms.ps1
+++ CreerVms.ps1
@@ -5,32 +5,30 @@ function Executer-VBoxCmde {
 
     Start-Process -NoNewWindow -FilePath "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" -ArgumentList "$pArguments"
 }
-
-<# 
+ 
 function Ajouter-Machine {
     Param (
-     [string] $NomMachine,
-     [string] $NomFichier,
-     [int] $RamMo,
-     [int] $NumPort,
-     [int] $NumDisque
+     [string] $pNomMachine,
+     [string] $pTypeOS,
+     [int] $pRamMo,
+     [string] $pRepertoireRacine
     )
-
-   VBoxManage modifyvm $NomMachine  --ioapic on --chipset ich9 --memory $RamMo --vram 128 --rtcuseutc on --boot1 dvd --boot2 disk
-   VBoxManage modifyvm $NomMachine --graphicscontroller vmsvga
-} #>
+    Executer-VBoxCmde "createvm --name $pNomMachine --ostype $pTypeOS --register --basefolder $pRepertoireRacine"
+    Executer-VBoxCmde "modifyvm $pNomMachine  --ioapic on --chipset ich9 --memory $pRamMo --vram 128 --rtcuseutc on --boot1 dvd --boot2 disk"
+    Executer-VBoxCmde "modifyvm $pNomMachine --graphicscontroller vmsvga"
+}
 
 function Ajouter-DisqueSata {
     Param (
-     [string] $NomMachine,
-     [string] $NomFichier,
-     [int] $TailleMo,
-     [int] $NumPort,
-     [int] $NumDisque
+     [string] $pNomMachine,
+     [string] $pNomFichier,
+     [int] $pTailleMo,
+     [int] $pNumPort,
+     [int] $pNumDisque
     )
-     write-host "VBoxManage createhd --filename "$NomFichier" --size "$TailleMo" --format VDI"
-     write-host "VBoxManage storagectl "$NomMachine" --name ""SATA Controller"" --add sata --controller IntelAhci"
-     write-host "VBoxManage storageattach "$NomMachine" --storagectl ""SATA Controller"" --port "$NumPort" --device "$NumDisque" --type hdd --medium  "$NomFichier
+    Executer-VBoxCmde "createhd --filename $pNomFichier --size $TailleMo --format VDI"
+    Eecuter-VBoxCmde "storagectl $pNomMachine --name ""SATA Controller"" --add sata --controller IntelAhci"
+    Executer-VBoxCmde "storageattach $pNomMachine --storagectl ""SATA Controller"" --port $pNumPort --device $pNumDisque --type hdd --medium $NomFichier"
 }
 
 # $ImportData = Import-Csv "modele_lin-orl.csv" -delimiter ";"
@@ -43,12 +41,8 @@ function Ajouter-DisqueSata {
 # $RamMo = 4096
 #Executer-VBoxCmde "createvm --name $NomMachine  --ostype Oracle_64 --register --basefolder H:\projets\VBox\Vms"
 #Lecture du fichier.
-[xml]$xml = get-content ("H:\projets\scripts\pws\lin-orl-002.xml")
-
-#Je récupéré dans $Livres les données entre la balise <Livres> et </Livres>.
+[xml]$xml = get-content (".\lin-orl-002.xml")
 $machine = $xml.machine
 
-#Puis j'affiche mes données.
-Write-Host $machine.nom
-Write-Host $machine.os_type
-Write-Host $machine.disques[0]
+Write-Host "Creation de la machine "$machine.nom "["$machine.os_type"] dans "$machine.repertoire_racine
+Ajouter-Machine $machine.nom $machine.os_type $machine.memoire_mo $machine.repertoire_racine
