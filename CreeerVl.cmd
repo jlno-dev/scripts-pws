055ba88703c9d3fcb9b75ee7da9726571e0195f3
jlno-dev <63147914+jlno-dev@users.noreply.github.com>
2021-06-09 18:37:08 +0200

    Create CreeerVl.cmd

---
+++ CreeerVl.cmd
@@ -0,0 +1,10 @@
+# $ImportData = Import-Csv "modele_lin-orl.csv" -delimiter ";"
+# foreach ( $Data in  $ImportData )
+# {
+# 	$fichier_complet = $Data.machine + "_" + $Data.fichier
+#      Ajouter-DisqueSata $Data.machine $fichier_complet $Data.taille $Data.port $Data.device
+# } 
+#$NomMachine = "lin-orl-002"
+# $RamMo = 4096
+#Executer-VBoxCmde "createvm --name $NomMachine  --ostype Oracle_64 --register --basefolder H:\projets\VBox\Vms"
+#Lecture du fichier.
